5615a28b36e7147c45ee05c7f3644dc1bee708d4
Bryan Saldivar <bsaldivar.emc2@gmail.com>
2023-08-16 10:51:08 +0200

    Update generate.py: Save results on file, not print
    Added log option for running in cluster.
    Results written in a file different from stdout. 
    Removal of duplicates

--- generate.py
+++ generate.py
@@ -11,6 +11,12 @@ from tqdm import tqdm
 
 from hgraph import *
 import rdkit
+import traceback
+import numpy as np
+import logging
+import os
+from datetime import datetime
+
 
 lg = rdkit.RDLogger.logger() 
 lg.setLevel(rdkit.RDLogger.CRITICAL)
@@ -33,23 +39,50 @@ parser.add_argument('--depthG', type=int, default=15)
 parser.add_argument('--diterT', type=int, default=1)
 parser.add_argument('--diterG', type=int, default=3)
 parser.add_argument('--dropout', type=float, default=0.0)
+parser.add_argument('--output_dir', type=str, default='generated')
+parser.add_argument('--log_name', type=str, default="generate_log")
 
-args = parser.parse_args()
 
-vocab = [x.strip("\r\n ").split() for x in open(args.vocab)] 
-args.vocab = PairVocab(vocab)
-
-model = HierVAE(args).cuda()
-
-model.load_state_dict(torch.load(args.model)[0])
-model.eval()
-
-torch.manual_seed(args.seed)
-random.seed(args.seed)
-
-with torch.no_grad():
-    for _ in tqdm(range(args.nsample // args.batch_size)):
-        smiles_list = model.sample(args.batch_size, greedy=True)
-        for _,smiles in enumerate(smiles_list):
-            print(smiles)
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print("Setting logger")
+    time_now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    _log_file = "{}_{}.log".format(args.log_name,time_now)
+    logging.basicConfig(filename=_log_file, filemode='a', format='%(name)s - %(levelname)s - %(message)s',level=10)
+    def log_msg(msg):
+        time_now = datetime.now().strftime("%Y%m%d-%H%M%S")
+        _m = "{}: {}.".format(time_now,msg)
+        print(_m)
+        logging.info(_m)
+    log_msg("Finished args parsing")
+    log_msg(args)
+    #
+    vocab = [x.strip("\r\n ").split() for x in open(args.vocab)] 
+    args.vocab = PairVocab(vocab)
+    log_msg("Vocab parsed")
+    model = HierVAE(args).cuda()
+    model.load_state_dict(torch.load(args.model)[0])
+    model.eval()
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    log_msg("Model loaded")
+    os.makedirs(args.output_dir,exist_ok=True)
+    output_file = os.path.join(args.output_dir,"generated_{}.txt".format(time_now))
+    output_smiles = {}
+    _batches = args.nsample // args.batch_size
+    with torch.no_grad():
+        #for _ in tqdm(range(args.nsample // args.batch_size)):
+        for _batch in range(_batches):
+            log_msg("Starting {}/{}".format(_batch,_batches-1))
+            smiles_list = model.sample(args.batch_size, greedy=True)
+            for _ in smiles_list:
+                c = output_smiles.get(_,0)
+                output_smiles[_]=c + 1
+    log_msg("Saving results in {}".format(output_file))
+    with open(output_file,"w") as f:
+        for smile,count in output_smiles.items():
+            _ = "{}\t{}\n".format(smile,count)
+            f.write(_)
+    log_msg("Done")
+        
 
